44ae980203dec75d1be9a6f4a54379299fb08582
EXERCISE12 <e.xercise125@protonmail.com>
2021-03-04 03:20:01 +0100

    added color and topgrade install section

--- simple_manjaro.sh
+++ simple_manjaro.sh
@@ -1,27 +1,30 @@
 #! /bin/bash 
 
+Yel='\e[1;33m'
 
-echo "Update system and package"
+echo -e "${Yel}Update System and Packages${Yel}"
 sudo pacman -Syyu
 
 
-echo "Install base-developer tools for manjaro"
+echo -e "${Yel}Install base-developer tools for manjaro${Yel}"
 sudo pacman -S base-devel
 
-echo "install ZSH Shell"
+echo -e "${Yel}Install ZSH Shell${Yel}"
 sudo pacman -Syu zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "install ZSH PLUGINS"
+echo -e "${Yel}install ZSH PLUGINS${Yel}"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
  
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo "instal non-free driver like (NVIDIA Driver)" 
+echo -e "${Yel}Install non-free driver like${Yel}" 
 sudo mhwd -a pci nonfree 0300
 
+echo -e "${Yel}Install Topgrade for update all System in one time"
+sudo pacman -S cargo 
+sudo cargo install topgrade 
 
-# 4 install cargo topgrade 
  
 # 5 install brave / pycharm / spotify 
 # yay bin-brave 
